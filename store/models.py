5d472cfd4eaca83a061cb97f4f383d689b2d0fdc
Kaio Masaru Nakazono <kmn1405.cg@gmail.com>
2023-02-15 08:27:16 -0300

    created migrations

--- store/models.py
+++ store/models.py
@@ -10,6 +10,7 @@ class Collection(models.Model):
 
 class Product(models.Model):
    title = models.CharField(max_length=255)
+   slug = models.SlugField()
    description = models.TextField()
    price = models.DecimalField(max_digits=6, decimal_places=2)
    inventory = models.IntegerField()
@@ -58,6 +59,7 @@ class OrderItem(models.Model):
 
 #Part 1 - Cap 3 - video 7
 class Address(models.Model):
+   zip = models.CharField(max_length=10)
    street = models.CharField(max_length=255)
    city = models.CharField(max_length=255)
    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
