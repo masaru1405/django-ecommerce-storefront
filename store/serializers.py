68fdcb95df2c3300e76d0c397880f75c32348dd7
Kaio Masaru Nakazono <kmn1405.cg@gmail.com>
2023-02-25 08:11:04 -0300

    serializing relationships

--- store/serializers.py
+++ store/serializers.py
@@ -1,12 +1,21 @@
 from decimal import Decimal
 from rest_framework import serializers
-from .models import Product
+from .models import Product, Collection
+
+class CollectionSerializer(serializers.Serializer):
+   id = serializers.IntegerField()
+   title = serializers.CharField(max_length=255)
 
 class ProductSerializer(serializers.Serializer):
    id = serializers.IntegerField()
    title = serializers.CharField(max_length=255)
    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2, source='price')
    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+   #collection =  CollectionSerializer()
+   collection = serializers.HyperlinkedRelatedField(
+      queryset = Collection.objects.all(),
+      view_name = 'collection-detail'
+   )
 
    def calculate_tax(self, product: Product):
       return product.price * Decimal(1.1)
