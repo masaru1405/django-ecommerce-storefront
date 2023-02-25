ffb89fed6a97bd49b7077ff108fce4f11d44f023
Kaio Masaru Nakazono <kmn1405.cg@gmail.com>
2023-02-25 08:24:41 -0300

    model serializers

--- store/serializers.py
+++ store/serializers.py
@@ -2,16 +2,18 @@ from decimal import Decimal
 from rest_framework import serializers
 from .models import Product, Collection
 
-class CollectionSerializer(serializers.Serializer):
-   id = serializers.IntegerField()
-   title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
+   class Meta:
+      model = Collection
+      fields = ['id', 'title']
 
-class ProductSerializer(serializers.Serializer):
-   id = serializers.IntegerField()
-   title = serializers.CharField(max_length=255)
+class ProductSerializer(serializers.ModelSerializer):
+   class Meta:
+      model = Product
+      fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
+  
    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2, source='price')
    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-   #collection =  CollectionSerializer()
    collection = serializers.HyperlinkedRelatedField(
       queryset = Collection.objects.all(),
       view_name = 'collection-detail'
