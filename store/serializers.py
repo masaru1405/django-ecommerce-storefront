fd525a2ae25cdfb7f33437aa85ec05f7ab3b3087
Kaio Masaru Nakazono <kmn1405.cg@gmail.com>
2023-03-11 08:22:00 -0300

    nested routers

--- store/serializers.py
+++ store/serializers.py
@@ -1,6 +1,6 @@
 from decimal import Decimal
 from rest_framework import serializers
-from .models import Product, Collection
+from .models import Product, Collection, Review
 
 class CollectionSerializer(serializers.ModelSerializer):
    class Meta:
@@ -28,5 +28,10 @@ class ReviewSerializer(serializers.ModelSerializer):
    class Meta:
       model = Review
       fields = ['id', 'date', 'name', 'description', 'product']
+   
+   def create(self, validated_data):
+      product_id = self.context['product_id']
+      return Review.objects.create(product_id=product_id, **validated_data)
+
 
 
--- store/urls.py
+++ store/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 from rest_framework.routers import SimpleRouter
+from rest_framework_nested import routers
 from . import views
 
-router = SimpleRouter()
+router = routers.DefaultRouter()
 router.register('products', views.ProductViewSet)
 router.register('collections', views.CollectionViewSet)
 
-urlpatterns = router.urls
+products_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
+products_router.register('reviews', views.ReviewViewSet, basename='product-reviews')
+
+#URLConf
+urlpatterns = router.urls + products_router.urls
 
 # urlpatterns = [
 #    path('products/', views.ProductList.as_view()),
