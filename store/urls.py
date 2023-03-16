6f15c89a6e20761759b00768e90de712ea6d2fcb
Kaio Masaru Nakazono <kmn1405.cg@gmail.com>
2023-03-15 22:34:49 -0300

    getting cart items

--- store/urls.py
+++ store/urls.py
@@ -9,8 +9,11 @@ router.register('carts', views.CartViewSet)
 products_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
 products_router.register('reviews', views.ReviewViewSet, basename='product-reviews')
 
+carts_routers = routers.NestedDefaultRouter(router, 'carts', lookup='cart')
+carts_routers.register('items', views.CartItemViewSet, basename='cart-items')
+
 #URLConf
-urlpatterns = router.urls + products_router.urls
+urlpatterns = router.urls + products_router.urls + carts_routers.urls
 
 # urlpatterns = [
 #    path('products/', views.ProductList.as_view()),
--- store/views.py
+++ store/views.py
@@ -6,8 +6,8 @@ from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework import status
 from rest_framework.filters import SearchFilter, OrderingFilter
 
-from .models import Product, Collection, OrderItem, Review, Cart
-from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer, CartSerializer
+from .models import Product, Collection, OrderItem, Review, Cart, CartItem
+from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer, CartSerializer, CartItemSerializer
 from .filters import ProductFilter
 
 class ProductViewSet(ModelViewSet):
@@ -135,4 +135,10 @@ class ReviewViewSet(ModelViewSet):
 class CartViewSet(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, GenericViewSet):
     queryset = Cart.objects.prefetch_related('items__product').all()
     serializer_class = CartSerializer
+
+class CartItemViewSet(ModelViewSet):
+    serializer_class = CartItemSerializer
+
+    def get_queryset(self):
+        return CartItem.objects.filter(cart_id=self.kwargs['cart_pk']).select_related('product')
 
